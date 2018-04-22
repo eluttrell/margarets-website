8756b6a10bcf8d085baf6b751c60ffeb60f54384
Elijah Allstrom-Luttrell <elieel@gmail.com>
2018-04-21 20:46:02 -0400

    Begin learning react

---
+++ src/components/work-item-button/work-item-button.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './work-item-button.css';
+
+class WorkItemButton extends Component {
+  log = function() {
+	  console.log("test");
+	};
+
+  render() {
+    return (
+      <div className="work-item-button">
+	
+     </div>
+    );
+  }
+}
+
+export default WorkItemButton;
