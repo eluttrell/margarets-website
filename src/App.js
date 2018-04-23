48c028e9535cbe7a86850ec957853a3062462136
eluttrell <elijah@elijahs-mbp.attlocal.net>
2018-04-22 23:20:52 -0400

    Begin work on main page layout

--- src/App.js
+++ src/App.js
@@ -5,10 +5,28 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="app-header"
-        <div className="app-container">
-            <WorkItemButton className="app-work-item-button"/>
+      <div className="app">
+        <header className="app-header">
+          <p>home</p>
+          <p>page1</p>
+          <p>page2</p>
+          <p>page3</p>
+          <p>page4</p>
+          <p>page5</p>
+          <p>page6</p>
+          <p>contact</p>
+        </header>
+        <div className="app-title">
+          <div className="app-title-main">Margaret E. Allstrom-Luttrell</div>
+          <small className="app-title-footer">Artist | Govorness</small>
+        </div> 
+        <div className="app-content">
+          <WorkItemButton className="app-content-work-item-button"/>
+          <WorkItemButton className="app-content-work-item-button"/>
+          <WorkItemButton className="app-content-work-item-button"/>
+          <WorkItemButton className="app-content-work-item-button"/>
+          <WorkItemButton className="app-content-work-item-button"/>
+          <WorkItemButton className="app-content-work-item-button"/>
         </div>
      </div>
     );
